bd5dc00d94012dbb14c51103ddb959da9a8a72c3
Marine Lancelin <lancelinmarine@gmail.com>
2026-04-30 12:27:27 +0200

    api-service: delete service api

--- app/Http/Controllers/ApiCoursController.php
+++ app/Http/Controllers/ApiCoursController.php
@@ -3,21 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Client\ConnectionException;
-use App\Services\ApiService;
+use Illuminate\Support\Facades\Http;
 
 class ApiCoursController extends Controller
 {
-    public function __construct(private readonly ApiService $api)
-    {
-    }
-
     /**
      * @throws ConnectionException
      */
     public function index()
     {
-        $response = $this->api->connect()
-            ->get('/cours');
+        $response = Http::baseUrl(config('services.api.url'))
+                    ->acceptJson()
+                    ->get('/cours');
 
         if ($response->failed()) {
             abort($response->status(), 'Impossible de récupérer les cours');
@@ -33,8 +30,9 @@ class ApiCoursController extends Controller
      */
     public function show($id)
     {
-        $response = $this->api->connect()
-            ->get("/cours/{$id}");
+        $response = Http::baseUrl(config('services.api.url'))
+                    ->acceptJson()
+                    ->get("/cours/{$id}");
 
         return view('signature', [
             'cours' => $response->json(),
